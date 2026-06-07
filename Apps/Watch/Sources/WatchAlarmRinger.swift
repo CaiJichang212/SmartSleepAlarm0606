08e312e20db6b1c9302b7500000d7b746ddbd580
陆张弛 <li_y_c@qq.com>
2026-06-07 19:19:44 +0800

    feat(手表端闹钟): 添加响铃控制的协议与实现
    定义了闹钟响铃控制的协议接口，同时提供了测试用的模拟实现和基于WatchKit Taptic反馈的真实生产实现，方便后续解耦与测试。

---
+++ Apps/Watch/Sources/WatchAlarmRinger.swift
@@ -0,0 +1,28 @@
+import Foundation
+import WatchKit
+
+protocol WatchAlarmRinging {
+    func startRinging()
+    func snooze()
+    func stop()
+}
+
+struct FakeWatchAlarmRinger: WatchAlarmRinging {
+    func startRinging() {}
+    func snooze() {}
+    func stop() {}
+}
+
+struct WatchAlarmRinger: WatchAlarmRinging {
+    func startRinging() {
+        WKInterfaceDevice.current().play(.notification)
+    }
+
+    func snooze() {
+        WKInterfaceDevice.current().play(.directionDown)
+    }
+
+    func stop() {
+        WKInterfaceDevice.current().play(.success)
+    }
+}
