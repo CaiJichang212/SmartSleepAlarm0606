a2266e2fa1ae9ccbf62b9dbe05b4de08f57e6cb6
陆张弛 <li_y_c@qq.com>
2026-06-07 17:09:45 +0800

    feat(AlarmRepository): 新增内存版闹钟数据仓库实现
    定义了AlarmRepository协议，同时实现了MemoryAlarmRepository作为内存存储的具体实现，支持闹钟的增删改查以及按时间排序查询。

---
+++ Packages/SmartSleepCore/Sources/SmartSleepCore/AlarmRepository.swift
@@ -0,0 +1,41 @@
+import Foundation
+
+public protocol AlarmRepository: Sendable {
+    func list() throws -> [Alarm]
+    func alarm(id: UUID) throws -> Alarm?
+    func save(_ alarm: Alarm) throws
+    func delete(id: UUID) throws
+}
+
+public final class MemoryAlarmRepository: AlarmRepository, @unchecked Sendable {
+    private var storage: [UUID: Alarm]
+
+    public init(alarms: [Alarm] = []) {
+        self.storage = Dictionary(uniqueKeysWithValues: alarms.map { ($0.id, $0) })
+    }
+
+    public func list() throws -> [Alarm] {
+        storage.values.sorted(by: Self.sort(lhs:rhs:))
+    }
+
+    public func alarm(id: UUID) throws -> Alarm? {
+        storage[id]
+    }
+
+    public func save(_ alarm: Alarm) throws {
+        storage[alarm.id] = alarm
+    }
+
+    public func delete(id: UUID) throws {
+        storage.removeValue(forKey: id)
+    }
+
+    private static func sort(lhs: Alarm, rhs: Alarm) -> Bool {
+        let lhsHour = lhs.timeOfDay.hour ?? 0
+        let rhsHour = rhs.timeOfDay.hour ?? 0
+        if lhsHour != rhsHour {
+            return lhsHour < rhsHour
+        }
+        return (lhs.timeOfDay.minute ?? 0) < (rhs.timeOfDay.minute ?? 0)
+    }
+}
