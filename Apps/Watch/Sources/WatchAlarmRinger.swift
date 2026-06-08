646b70b31250571cb042231baa4422b1e85d2fe7
陆张弛 <li_y_c@qq.com>
2026-06-08 19:48:00 +0800

    refactor(WatchAlarmRinger): 重构FakeWatchAlarmRinger并添加调用计数
    将原结构体改为类，新增调用次数统计属性，拆分实现方法

--- Apps/Watch/Sources/WatchAlarmRinger.swift
+++ Apps/Watch/Sources/WatchAlarmRinger.swift
@@ -7,10 +7,22 @@ protocol WatchAlarmRinging {
     func stop()
 }
 
-struct FakeWatchAlarmRinger: WatchAlarmRinging {
-    func startRinging() {}
-    func snooze() {}
-    func stop() {}
+final class FakeWatchAlarmRinger: WatchAlarmRinging {
+    private(set) var startCallCount = 0
+    private(set) var snoozeCallCount = 0
+    private(set) var stopCallCount = 0
+
+    func startRinging() {
+        startCallCount += 1
+    }
+
+    func snooze() {
+        snoozeCallCount += 1
+    }
+
+    func stop() {
+        stopCallCount += 1
+    }
 }
 
 struct WatchAlarmRinger: WatchAlarmRinging {
