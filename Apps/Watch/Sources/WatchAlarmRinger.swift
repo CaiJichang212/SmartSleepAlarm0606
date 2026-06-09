4f3b737c42283c51fb65cdf57170a8d8dd7241ff
陆张弛 <li_y_c@qq.com>
2026-06-09 16:03:00 +0800

    refactor(WatchAlarmRinger): remove fake test implementation
    删除了无用的FakeWatchAlarmRinger测试类，清理冗余代码

--- Apps/Watch/Sources/WatchAlarmRinger.swift
+++ Apps/Watch/Sources/WatchAlarmRinger.swift
@@ -7,24 +7,6 @@ protocol WatchAlarmRinging {
     func stop()
 }
 
-final class FakeWatchAlarmRinger: WatchAlarmRinging {
-    private(set) var startCallCount = 0
-    private(set) var snoozeCallCount = 0
-    private(set) var stopCallCount = 0
-
-    func startRinging() {
-        startCallCount += 1
-    }
-
-    func snooze() {
-        snoozeCallCount += 1
-    }
-
-    func stop() {
-        stopCallCount += 1
-    }
-}
-
 struct WatchAlarmRinger: WatchAlarmRinging {
     func startRinging() {
         WKInterfaceDevice.current().play(.notification)
